cab1cf0939aa28cfaf6dc5860836d604e0faf1ca
Konrad <kocasp@gmail.com>
2025-01-04 13:39:24 +0100

    Add crawled db and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,23 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'csv'
+
+puts "Loading products from CSV..."
+
+return puts "Products already exist in database, skipping seed" if Product.count > 0
+
+count = 0
+total_rows = CSV.read(Rails.root.join('db/db.csv')).length
+CSV.foreach(Rails.root.join('db/db.csv'), headers: false) do |row|
+  Product.create!(
+    id: row[0],
+    url_id: row[1],
+    name: row[2],
+    kcal: row[3],
+    protein: row[4], 
+    carbs: row[5],
+    fat: row[6],
+    fiber: row[7]
+  )
+  count += 1
+  puts "Processed #{(count.to_f / total_rows * 100).round(1)}% (#{count}/#{total_rows} products)..." if count % 100 == 0
+end
+puts "Created #{Product.count} products"
