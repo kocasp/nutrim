58b60b9f2f53a6d83b18ae6afd4c21de66166245
Konrad <kocasp@gmail.com>
2025-01-28 20:35:45 +0100

    Change response amount

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ProductsController < ApplicationController
   api :GET, '/products', 'Pobierz listę produktów'
   param :query, String, desc: 'Slowo kluczowe, które Nutrim będzie wyszukiwal w swojej bazie produktów.', required: false
   def index
-    products = Product.search(params[:query]).first(20)
+    products = Product.search(params[:query]).first(100)
     render json: products, status: :ok
   end
 end
