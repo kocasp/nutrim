965a93835c32a046d44903f9c273fac5696aecad
Konrad <kocasp@gmail.com>
2025-02-02 20:18:21 +0100

    Enable no accents

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::ProductsController < ApplicationController
   api :GET, '/products', 'Pobierz listę produktów'
   param :query, String, desc: 'Slowo kluczowe, które Nutrim będzie wyszukiwal w swojej bazie produktów.', required: false
   def index
-    products = Product.search_name(params[:query]).limit(20)
+    no_accent_query = I18n.transliterate(params[:query])
+    products = Product.search_name(no_accent_query).limit(20)
     render json: products, status: :ok
   end
 end
